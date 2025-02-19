4b88185a8fd87eb9d9014f82a344f2c3176c8028
jslootsky <joshuaslootsky@gmail.com>
2025-02-19 15:34:36 -0500

    Changed the getSize function from the adapter to return the size of the array rather than just a hardcoded value
    Also modified the onClickListener for the delete button

--- app/src/main/java/edu/temple/namelist/MainActivity.kt
+++ app/src/main/java/edu/temple/namelist/MainActivity.kt
@@ -37,8 +37,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<View>(R.id.deleteButton).setOnClickListener {
-            (names as MutableList).removeAt(spinner.selectedItemPosition)
-            (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+            if(names.isNotEmpty()) {
+                (names as MutableList).removeAt(spinner.selectedItemPosition)
+                (spinner.adapter as BaseAdapter).notifyDataSetChanged()
+                nameTextView.text = names[0]
+            }
         }
 
     }
